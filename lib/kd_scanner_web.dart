16f50920cde8a0d263bb00ac6ec57c57cebc58f0
kokemus <>
2021-01-02 18:43:48 +0200

    nullsafety

--- lib/kd_scanner_web.dart
+++ lib/kd_scanner_web.dart
@@ -5,22 +5,21 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:kd_scanner/kd_scanner_platform_interface.dart';
 
 class KdScannerPlugin extends KdScannerPlatform {
-
-  BarcodeScanner _barcodeScanner;
+  late BarcodeScanner _barcodeScanner;
 
   static void registerWith(Registrar registrar) async {
     await BarcodeScannerFactory.loadScript();
-    KdScannerPlatform.instance = KdScannerPlugin();    
+    KdScannerPlatform.instance = KdScannerPlugin();
   }
 
   KdScannerPlugin() {
-    _barcodeScanner = BarcodeScanner(
-      BarcodeScannerOptions(formats: ['code_39', 'code_128', 'ean_13', 'qr_code'])
-    );
+    _barcodeScanner = BarcodeScanner(BarcodeScannerOptions(
+        formats: ['code_39', 'code_128', 'ean_13', 'qr_code']));
   }
 
   @override
-  Future<String> get platformVersion => Future.value(window.navigator.userAgent);
+  Future<String> get platformVersion =>
+      Future.value(window.navigator.userAgent);
 
   @override
   Future<String> scan() {
